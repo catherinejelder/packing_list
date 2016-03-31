31c131f19574423528cded4f1f5273e149477b7e
catherinejelder <catherinejelder@gmail.com>
2016-03-31 02:31:41 -0400

    enabling multithreading

--- datastore.py
+++ datastore.py
@@ -1,19 +1,21 @@
 import re
 from collections import defaultdict
 from util import Message, InputError
-
+import threading
 # self.dependency_map = {}
 # self.dependents_count = defaultdict(int)
 
 class Datastore(object):
     def __init__(self, dependency_map = {}, dependents_count = defaultdict(int)):
+        self.lock = threading.Lock()
         self.dependency_map = dependency_map
         self.dependents_count = dependents_count
         print ('datastore __init__ called')
 
     def parse_message(self, message):
         # print ('parse_message called with message', message)
-        match = re.match('(INDEX|REMOVE|QUERY)\|([\w-]+)\|([,\w-]*)\\n$', message)
+        # match = re.match('(INDEX|REMOVE|QUERY)\|([\w-]+)\|([,\w-]*)\\n$', message)
+        match = re.match('(INDEX|REMOVE|QUERY)\|([+\w-]+)\|([,+\w-]*)\\n$', message)
         if not match:
             raise InputError(message)
         command, package, dependency_list = match.group(1), match.group(2), match.group(3).split(',')
@@ -43,33 +45,36 @@ class Datastore(object):
         return self.query_package(package)
 
     def index_package(self, package, dependency_list):
-        if package in self.dependency_map:
+        with self.lock:
+            if package in self.dependency_map:
+                return str(Message.OK)
+            for dependency in dependency_list:
+                if dependency not in self.dependency_map:
+                    return str(Message.Fail)
+            self.dependency_map[package] = dependency_list
+            for dependency in dependency_list:
+                self.dependents_count[dependency] += 1
             return str(Message.OK)
-        for dependency in dependency_list:
-            if dependency not in self.dependency_map:
-                return str(Message.Fail)
-        self.dependency_map[package] = dependency_list
-        for dependency in dependency_list:
-            self.dependents_count[dependency] += 1
-        return str(Message.OK)
 
     def remove_package(self, package, dependency_list):
         # print ('before removing anything', 'dependency_map', self.dependency_map, 'dependents_count', self.dependents_count)
-        if package not in self.dependency_map:
+        with self.lock:
+            if package not in self.dependency_map:
+                return str(Message.OK)
+            if package in self.dependents_count:
+                return str(Message.Fail)
+            for dependency in self.dependency_map[package]:
+                # print ('dealing with dependency', dependency)
+                if self.dependents_count[dependency] == 1:
+                    del self.dependents_count[dependency]
+                else:
+                    self.dependents_count[dependency] -= 1
+            del self.dependency_map[package] # TODO: tackle concurrent access by multiple clients to data store
+            # print ('after removal', 'dependency_map', self.dependency_map, 'dependents_count', self.dependents_count)
             return str(Message.OK)
-        if package in self.dependents_count:
-            return str(Message.Fail)
-        for dependency in self.dependency_map[package]:
-            # print ('dealing with dependency', dependency)
-            if self.dependents_count[dependency] == 1:
-                del self.dependents_count[dependency]
-            else:
-                self.dependents_count[dependency] -= 1
-        del self.dependency_map[package] # TODO: tackle concurrent access by multiple clients to data store
-        # print ('after removal', 'dependency_map', self.dependency_map, 'dependents_count', self.dependents_count)
-        return str(Message.OK)
 
     def query_package(self, package):
-        if package in self.dependency_map:
-            return str(Message.OK)
-        return str(Message.Fail)
+        with self.lock:
+            if package in self.dependency_map:
+                return str(Message.OK)
+            return str(Message.Fail)
--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 import socketserver
 from datastore import Datastore
+import logging
+import threading
 
-class MyTCPServer(socketserver.TCPServer):
+logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s %(message)s')
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     request_queue_size = 100
 
 
-class TCPHandler(socketserver.StreamRequestHandler):
+class ThreadedTCPHandler(socketserver.StreamRequestHandler):
     """
     The request handler class for our server.
 
@@ -16,18 +20,18 @@ class TCPHandler(socketserver.StreamRequestHandler):
     def handle(self):
         # self.datastore = get_datastore_ref()
         # print('type of self.rfile', type(self.rfile))
+        cur_thread = threading.current_thread()
         for line in self.rfile:
             # self.data = self.rfile.readline()
 
             # print("{} wrote:".format(self.client_address[0]))
             # print(self.data)
 
-            # request_str = self.data.decode('ascii')
-            request_str = line.decode('ascii')
-            print('request_str', request_str)
+            request_str = line.decode('utf-8')
+            logging.info('from port ' + str(self.client_address[1]) + ', request_str ' + request_str)
             reply_str = get_datastore_ref().process_message(request_str)
-            print('reply_str', reply_str)
-            reply_bytes = reply_str.encode('ascii')
+            logging.info('thread ' + cur_thread.name + ', for port ' + str(self.client_address[1]) + ', reply_str ' + reply_str)
+            reply_bytes = reply_str.encode('utf-8')
             self.wfile.write(reply_bytes)
 
 
@@ -40,5 +44,5 @@ def get_datastore_ref():
 if __name__ == "__main__":
     # HOST, PORT = "localhost", 8080
     HOST, PORT = "0.0.0.0", 8080
-    server = MyTCPServer((HOST, PORT), TCPHandler)
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPHandler)
     server.serve_forever()
