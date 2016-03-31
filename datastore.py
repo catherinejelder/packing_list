f367f1201f10b962b2f91630ab30f221964aa7dd
catherinejelder <catherinejelder@gmail.com>
2016-03-31 03:47:27 -0400

    cleanup

--- datastore.py
+++ datastore.py
@@ -4,8 +4,6 @@ import threading
 from collections import defaultdict
 from util import Message, InputError
 
-logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s %(message)s')
-
 class Datastore(object):
     def __init__(self, dependency_map = {}, dependents_count = defaultdict(int)):
         # TODO: achieve better performance via a reader-writer lock. any number of concurrent reads could be allowed, if no concurrent write is occurring.
@@ -14,7 +12,6 @@ class Datastore(object):
         self.dependents_count = dependents_count
 
     def parse_message(self, message):
-        logging.debug('parse_message called with message ' + message)
         # TODO: improve regex to filter out non syntactially correct dependency lists
         match = re.match('(INDEX|REMOVE|QUERY)\|([+\w-]+)\|([,+\w-]*)\\n$', message)
         if not match:
@@ -22,7 +19,6 @@ class Datastore(object):
         command, package, dependency_list = match.group(1), match.group(2), match.group(3).split(',')
         if dependency_list == ['']:
             dependency_list = []
-        logging.debug('command: ' + command + ', package: ' + package + ', dependency_list: ' + str(dependency_list))
         # test for unnecessary existence of dependency_list
         if command in ['REMOVE', 'QUERY'] and dependency_list:
             raise InputError(message)
@@ -32,9 +28,6 @@ class Datastore(object):
         return command, package, dependency_list
 
     def process_message(self, message):
-        logging.debug('processing message ' + message)
-        logging.debug('self.dependency_map' + str(self.dependency_map))
-        logging.debug('self.dependents_count ' + str(self.dependents_count))
         try:
             command, package, dependency_list = self.parse_message(message)
         except InputError:
